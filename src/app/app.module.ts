2a4aca2c328997759fc6d3891c406e8831c032e4
Mario Martínez L <mmartinezl@ucenfotec.ac.cr>
2022-06-02 22:21:47 -0600

    se sube el primer commit del formulario de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  @ViewChild('f', { static: false }) registrar!: NgForm;
+  nicknameExiste = true;
+  provincias: string [] = ['Cartago', 'San José', 'Heredia'];
+  defaultProvincia = "Cartago";
+  apodo = '';
+  contrasenia = '';
+  generos: string [] = ['masculino', 'femenino'];
+
+
+
+
+
+  
+
+  
+
+  // nombre = this.registrar.value.username;
+  // apellido1 = this.registrar.value.apellido1;
+  // apellido2 = this.registrar.value.apellido2;
+  // nickname = this.registrar.value.nickname;
+  // correo = this.registrar.value.email;
+
+
+
+  // @ViewChild('f', {static: false}) registrare: NgForm;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  //   const nombre = form.value.username;
+  //   const apellido1 = form.value.apellido1;
+  //   const apellido2 = form.value.apellido2;
+  //   const nickname = form.value.nickname;
+  //   console.log(nombre, apellido1, apellido2, nickname);
+  // }
+  onSubmit(){
+    console.log(this.registrar);
+    const nombre = this.registrar.value.username;
+    console.log(nombre);
+    console.log(this.registrar.controls.apellido1.status);
+    console.log(this.registrar.controls.apellido1.touched);
+  }
+}
